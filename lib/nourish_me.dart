90c43a774a41428fb76fb94cde385808621a45cb
Mohamed Emadx <MohamedElbakrawy70@gmail.com>
2024-06-01 16:24:57 +0300

    "Routes.splash"

--- lib/nourish_me.dart
+++ lib/nourish_me.dart
@@ -16,7 +16,7 @@ class NourishMe extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, _) => MaterialApp(
-        initialRoute: Routes.continueRegisterScreen,
+        initialRoute: Routes.splash,
         onGenerateRoute: AppRoutes().generateRoute,
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
