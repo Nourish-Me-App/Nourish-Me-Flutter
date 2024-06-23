7ddfffea1ad9358260f974be2145e07854d440cc
Mohamed Emadx <MohamedElbakrawy70@gmail.com>
2024-06-23 11:39:11 +0300

    "start app from splach "

--- lib/feature/auth/logic/cubit/data_screen_cubit.dart
+++ lib/feature/auth/logic/cubit/data_screen_cubit.dart
@@ -14,9 +14,6 @@ class DataScreenCubit extends Cubit<DataScreenState> {
   int ageCounter = 18;
   int weightCounter = 50;
   int heightCounter = 160;
-  Timer? _timerAge;
-  Timer? _timerHeight;
-  Timer? _timerWeight;
 
   Future<void> continueRegister(String path, dynamic data) async {
     emit(ContinueRegisterLoading());
@@ -117,51 +114,5 @@ class DataScreenCubit extends Cubit<DataScreenState> {
 
 
 
-  void startTimerIncreaseHeight() {
-    _timerHeight = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      if (heightCounter < 210) {
-        heightCounter++;
-        emit(IncreamentCounter());
-      }
-    });
-  }
-
-  void startTimerIncreaseWeight() {
-    _timerWeight = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      if (weightCounter < 160) {
-        weightCounter++;
-        emit(IncreamentCounter());
-      }
-    });
-  }
-
-
-
-  void startTimerDecreaseHeight() {
-    _timerHeight = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      if (heightCounter > 160) {
-        heightCounter--;
-        emit(DecreamentCounter());
-      }
-    });
-  }
-
-  void startTimerDecreaseWeight() {
-    _timerWeight = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      if (weightCounter > 50) {
-        weightCounter--;
-        emit(DecreamentCounter());
-      }
-    });
-  }
-
-
-
-  void stopTimerWeight() {
-    _timerWeight?.cancel();
-  }
-
-  void stopTimerHeight() {
-    _timerHeight?.cancel();
-  }
+  
 }
