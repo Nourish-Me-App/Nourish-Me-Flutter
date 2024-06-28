02a55124b1a63160e904edc6be3dafb330ebd5a3
Mohamed Emadx <MohamedElbakrawy70@gmail.com>
2024-06-28 19:27:25 +0300

    "after pull"

--- lib/nourish_me.dart
+++ lib/nourish_me.dart
@@ -16,7 +16,7 @@ class NourishMe extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, _) => MaterialApp(
-        initialRoute: Routes.continueRegisterScreen,
+        initialRoute: Routes.bottomNavBar,
         onGenerateRoute: AppRoutes().generateRoute,
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
