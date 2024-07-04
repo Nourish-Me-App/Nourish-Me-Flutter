a5786b1c83774a1943a6257a9c70b4bcc76a2faf
Mohamed Elshamy <elshamy15.mohamed@gmail.com>
2024-07-04 16:54:50 +0300

    - Fixed imports

--- lib/feature/workout/view/screens/times_up_screen.dart
+++ lib/feature/workout/view/screens/times_up_screen.dart
@@ -1,12 +1,12 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nourish_me/core/imports/verification_screen_imports.dart';
 import 'package:nourish_me/feature/workout/data/model/workout_model.dart';
 import 'package:nourish_me/feature/workout/view/screens/details_screen.dart';
-import '../../../../core/imports/app_routes_imports.dart';
-import '../../../../core/imports/login_imports.dart';
+import '../../../../core/helpers/app_images.dart';
+import '../../../../core/helpers/helper_methods.dart';
 import '../../../../core/theme/app_colors.dart';
+import '../../../../core/theme/app_text_styles.dart';
 
 class TimesUpScreen extends StatefulWidget {
   const TimesUpScreen({
